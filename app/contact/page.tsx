7cb4314b1e72c9a98d3a487b6756507f67c78e83
Osaze-love <loveomobude23@gmail.com>
2025-12-23 15:14:08 +0100

    about and contact page build

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, type MotionProps } from "framer-motion";
 import React from "react";
 
 const ContactPage = () => {
-	const fade = {
+	const fade: MotionProps = {
 		initial: { opacity: 0, y: 30 },
 		whileInView: { opacity: 1, y: 0 },
 		viewport: { once: true, amount: 0.2 },
